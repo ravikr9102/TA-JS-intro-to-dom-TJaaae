b91094254abf68270c8bc23ec5d3b91d8f935849
Ravi kumar <rkravikr9102@gmail.com>
2022-05-03 00:38:24 +0530

    changes

--- block-BJaaeh/code/script.js
+++ block-BJaaeh/code/script.js
@@ -1,8 +1,8 @@
 setInterval(() => {
     d = new Date();
     htime = d.getHours();
-    htime = d.getMinutes();
-    htime = d.getSeconds();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
     hrotation = 30*htime + mtime/2;
     mrotation = 6*mtime;
     srotation = 6*stime;
